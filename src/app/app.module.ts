857cf0be03a5f3c0e7dbfe60a05f6c834782a1e9
hoi <toilahoi007@gmail.com>
2018-09-30 10:46:34 +0700

    add home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,45 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule, Title} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {AlertModule} from 'ngx-bootstrap';
+import {AppComponent} from './root/app.component';
+import {NavigationComponent} from './navigation/navigation.component';
+import {HomeComponent} from './home/containerHome/home.component';
+import {ResumeComponent} from './resume/resume.component';
+import {ContactComponent} from './contact/contact.component';
+import { InfoComponent } from './home/info/info.component';
+import { InfoCardComponent } from './home/info-card/info-card.component';
 
-import { AppComponent } from './app.component';
+
+const routes: Routes = [
+
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent, data: {title: 'home'}},
+  {path: 'resume', component: ResumeComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: '**', redirectTo: 'home'},
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    HomeComponent,
+    ResumeComponent,
+    ContactComponent,
+    InfoComponent,
+    InfoCardComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    AlertModule.forRoot()
   ],
-  providers: [],
+  providers: [
+    Title
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
