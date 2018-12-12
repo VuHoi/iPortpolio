f056e74c7c0cc0047840c1a8ae091c298a664fc2
hoideptrai123 <toilahoi007@gmail.com>
2018-12-12 19:50:05 +0700

    binding project

--- src/app/components/project-card/project-card.component.ts
+++ src/app/components/project-card/project-card.component.ts
@@ -1,7 +1,8 @@
-import {Component, Input, OnInit, EventEmitter, Output} from '@angular/core';
-import {transition, trigger, useAnimation} from '@angular/animations';
-import {CardProjectEnter, CardHoverEnter, CardHoverLeave, CardLeave} from '../Animation/card-animate';
+import { Component, Input, OnInit, EventEmitter, Output } from '@angular/core';
+import { transition, trigger, useAnimation } from '@angular/animations';
+import { CardProjectEnter, CardHoverEnter, CardHoverLeave, CardLeave } from '../Animation/card-animate';
 import { Project } from 'src/app/models/project';
+import { SharedService } from 'src/app/shares/SharedService';
 
 @Component({
   // tslint:disable-next-line:component-selector
@@ -28,12 +29,18 @@ import { Project } from 'src/app/models/project';
 export class ProjectCardComponent implements OnInit {
   @Input() project: Project;
   @Output() infoChange = new EventEmitter();
+  isModify = false;
   stateHover = 'leave';
-  constructor() { }
+  constructor(private sharedService: SharedService) {
+    this.sharedService.getMessage().subscribe(data => this.isModify = data);
+  }
 
   ngOnInit() {
   }
   togleStateAnimation(state) {
     this.stateHover = state;
+  }
+  changeValueInput() {
+    this.infoChange.emit(this.project);
   }
 }
