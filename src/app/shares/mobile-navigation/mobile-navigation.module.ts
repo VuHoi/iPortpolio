cba7cb73284075cec4d9c7210b97e51a030b1ad8
hoideptrai123 <toilahoi007@gmail.com>
2019-01-12 13:09:26 +0700

    deploy

--- src/app/shares/mobile-navigation/mobile-navigation.module.ts
+++ src/app/shares/mobile-navigation/mobile-navigation.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MobileNavigationComponent } from './mobile-navigation.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   declarations: [MobileNavigationComponent],
   exports: [MobileNavigationComponent]
